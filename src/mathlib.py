9abdf41771d3abc953dc67d1a622fda7da72f682
vurmajs <62237610+vurmajs@users.noreply.github.com>
2020-04-08 21:23:43 +0200

    Create mathlib.py

---
+++ src/mathlib.py
@@ -0,0 +1,16 @@
+#   
+#   Author: Krystof Vurm    xvurmk00
+#   
+#   
+
+def add(num1, num2):
+    return int(num1) + int(num2)
+
+def sub(num1, num2):
+    return int(num1) - int(num2)
+
+def mul(num1, num2):
+    return int(num1) * int(num2)
+
+def div(num1, num2):
+    return int(num1) / int(num2)
