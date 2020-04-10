e76bb133aaf4584aae7a959bbaeafde063e47759
vurmajs <62237610+vurmajs@users.noreply.github.com>
2020-04-10 12:01:47 +0200

    added few tests

--- src/mathlib.py
+++ src/mathlib.py
@@ -1,16 +1,23 @@
 #   
 #   Author: Krystof Vurm    xvurmk00
-#   
-#   
+#   date: 10. 04. 2020
+#   desc: basic math functions
+#
 
 def add(num1, num2):
-    return int(num1) + int(num2)
+    return float(num1) + float(num2)
 
 def sub(num1, num2):
-    return int(num1) - int(num2)
+    return float(num1) - float(num2)
 
 def mul(num1, num2):
-    return int(num1) * int(num2)
+    return float(num1) * float(num2)
 
 def div(num1, num2):
-    return int(num1) / int(num2)
+
+	if float(num2) == 0:
+		raise ZeroDivisionError
+
+	result = float(num1) / float(num2)
+	return result
+	
