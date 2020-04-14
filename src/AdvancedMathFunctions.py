345e12cbda473472cb85acebc2339ae1a59a1209
xrosec00 <62237706+xrosec00@users.noreply.github.com>
2020-04-14 11:56:04 +0200

    Update AdvancedMathFunctions.py

--- src/AdvancedMathFunctions.py
+++ src/AdvancedMathFunctions.py
@@ -3,8 +3,37 @@ def power(x):
     return x
 
 def factorial(x):
-    y = x - 1
-    while y > 1:
-        x = x * y
-        y -= 1
-    return x
+	if x <= 0:
+		return - 1 #error, cant do factorial of neg numbers in real numbers
+	else:
+		y = x - 1
+		while y > 1:
+			x = x * y
+			y -= 1
+		return x
+
+def nroot_rec( minimum, maximum, prevMid, prec, expo, origNum):
+	middle=(maximum+minimum)/2
+	prec+=1
+
+	if (prevMid == middle):
+		return middle
+
+	if (prec < 200):
+		if middle**expo > origNum:
+			maximum=middle
+			return nroot_rec( minimum, maximum, middle, prec, expo, origNum)
+
+		elif middle**expo < origNum:
+			minimum=middle
+			return nroot_rec( minimum, maximum, middle, prec, expo, origNum)
+		elif middle**expo == origNum:
+			return middle
+	else:
+		return middle
+
+def nroot( num1, expo):
+	if num1 < 0:
+		return - 1 #error, cant do root of neg number in real numbers
+	else:
+		return nroot_rec( 0, num1, -1, 1, expo, num1)
